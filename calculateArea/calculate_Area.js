87585f0c812b1d775400bbb0ac3b2b309101863a
Radrock7 <ryansheahedu@gmail.com>
2025-08-06 08:05:16 -0400

    Groceries

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,5 +1,6 @@
 let length;
 let width;
+let groc1, groc2, groc3;
 function calculateArea() {
     //parseFloat() function converts string value retrieved from input field to a floating-point unmber
     length = parseFloat(document.getElementById('length').value);
@@ -8,3 +9,10 @@ function calculateArea() {
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
 
+function calculateTotal() {
+    groc1 = parseFloat(document.getElementById('grocery1').value);
+    groc2 = parseFloat(document.getElementById('grocery2').value);
+    groc3 = parseFloat(document.getElementById('grocery3').value);
+    let total_Amount = groc1 + groc2 + groc3;
+    document.getElementById('result2').innerText = `The total amount is: ${total_Amount}`;
+}
